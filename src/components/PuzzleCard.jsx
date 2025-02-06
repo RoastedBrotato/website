093e6c22c837fe554510275b4764db672b37601f
RoastedBrotato <waleedajaz1996@gmail.com>
2025-02-06 14:55:21 +0300

    feat: enhance PuzzleCard component with new position options and update layout in index page

--- src/components/PuzzleCard.jsx
+++ src/components/PuzzleCard.jsx
@@ -1,42 +1,68 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const PuzzleCard = ({ position = "top", className = "", children, ...props }) => {
-  const positionClasses = {
+const PuzzleCard = ({
+  position = "middle", // Default to middle
+  className = "",
+  children,
+  ...props
+}) => {
+  const baseStyles = `
+    relative overflow-visible
+    before:content-[''] before:absolute before:block
+    before:w-[15%] before:aspect-square
+    after:content-[''] after:absolute after:block
+    after:w-[15%] after:aspect-square
+  `;
+
+  const positionStyles = {
     top: `
-      mb-8 
-      relative overflow-visible
-      after:content-[''] after:absolute after:block
-      after:w-20 after:h-20
+      rounded-2xl mb-8
+      after:bg-light dark:after:bg-dark after:rounded-full
       after:left-1/2 after:-translate-x-1/2
       after:bottom-0 after:translate-y-1/2
-      after:bg-light dark:after:bg-dark  after:rounded-full
     `,
     bottom: `
-      mt-8 
-      relative overflow-visible
-      before:content-[''] before:absolute before:block
-      before:w-20 before:h-20
+      rounded-2xl mt-8
+      before:bg-[#4A1818] before:rounded-full
       before:left-1/2 before:-translate-x-1/2
       before:top-0 before:-translate-y-1/2
+    `,
+    left: `
+      rounded-2xl mr-8
+      after:bg-[#4A1818] after:rounded-full
+      after:top-1/2 after:-translate-y-1/2
+      after:right-0 after:translate-x-1/2
+    `,
+    right: `
+      rounded-2xl ml-8
       before:bg-[#4A1818] before:rounded-full
+      before:top-1/2 before:-translate-y-1/2
+      before:left-0 before:-translate-x-1/2
+    `,
+    middle: `
+      rounded-2xl my-8 mx-8
+      before:bg-[#4A1818] before:rounded-full
+      before:left-1/2 before:-translate-x-1/2
+      before:top-0 before:-translate-y-1/2
+      after:bg-light dark:after:bg-dark after:rounded-full
+      after:left-1/2 after:-translate-x-1/2
+      after:bottom-0 after:translate-y-1/2
     `,
   };
 
   return (
-    <div className="relative">
-      <div
-        className={`w-full max-w-2xl bg-[#4A1818] text-white p-8 ${positionClasses[position]} ${className}`}
-        {...props}
-      >
-        <div className="relative z-20">{children}</div>
-      </div>
+    <div
+      className={`w-full max-w-2xl bg-[#4A1818] text-white p-8 ${baseStyles} ${positionStyles[position]} ${className}`}
+      {...props}
+    >
+      <div className="relative z-20">{children}</div>
     </div>
   );
 };
 
 PuzzleCard.propTypes = {
-  position: PropTypes.oneOf(["top", "bottom"]),
+  position: PropTypes.oneOf(["top", "bottom", "left", "right", "middle"]),
   className: PropTypes.string,
   children: PropTypes.node,
 };
